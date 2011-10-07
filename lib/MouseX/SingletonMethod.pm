88431d0235d72b0ac34c0b7fe2c8959e6b288515
hideo55 <hide.o.j55@gmail.com>
2011-10-07 23:23:04 +0900

    initial commit

---
+++ lib/MouseX/SingletonMethod.pm
@@ -0,0 +1,32 @@
+package MouseX::SingletonMethod;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MouseX::SingletonMethod -
+
+=head1 SYNOPSIS
+
+  use MouseX::SingletonMethod;
+
+=head1 DESCRIPTION
+
+MouseX::SingletonMethod is
+
+=head1 AUTHOR
+
+Hideaki Ohno E<lt>hide.o.j55 {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
