40ef4461658a8436cab2f502ef1f3c79e7e7b483
hideo55 <hide.o.j55@gmail.com>
2011-10-08 20:34:21 +0900

    Initial commit

--- lib/MouseX/SingletonMethod.pm
+++ lib/MouseX/SingletonMethod.pm
@@ -1,22 +1,84 @@
 package MouseX::SingletonMethod;
 use strict;
 use warnings;
+
+use Mouse ();
+use Mouse::Exporter;
+use Mouse::Util::MetaRole;
+
 our $VERSION = '0.01';
+
+Mouse::Exporter->setup_import_methods( also => 'Mouse' );
+
+sub init_meta {
+    shift;
+    my %options = @_;
+
+    my $meta = Mouse->init_meta(%options);
+
+    Mouse::Util::MetaRole::apply_base_class_roles(
+        for_class => $options{for_class},
+        roles     => ['MouseX::SingletonMethod::Role'],
+    );
+    
+    return $meta;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-MouseX::SingletonMethod -
+MouseX::SingletonMethod - Mouse with Singleton Method facility
+
 
 =head1 SYNOPSIS
 
+  package Foo;
   use MouseX::SingletonMethod;
+  no MouseX::Singleton;
+  
+  package main;
+  my $foo1 = Foo->new;
+  my $foo2 = Foo->new;
+  
+  $foo1->add_singleton_method( foo => sub { 'foo' } );
+  
+  say $foo1->foo; # => 'foo'
+  say $foo2->foo; # ERROR: Can't locate object method "foo" ...
+
+or
+
+  package Bar;
+  use Mouse;
+  with 'MouseX::SingletonMethod::Role';
+
+  no Mouse;
 
 =head1 DESCRIPTION
 
-MouseX::SingletonMethod is
+This module can create singleton methods with Mouse.
+
+=head1 METHODS
+
+=head2 become_singleton
+
+Make the object a singleton
+
+=head2 add_singleton_method
+
+Adds a singleton method to this object:
+
+  $foo->add_singleton_method( foo => sub { 'foo' } );
+
+=head2 add_singleton_methods
+
+Same as above except allows multiple method declaration:
+
+  $bar->add_singleton_methods(
+      bar1 => sub { 'bar1' },
+      bar2 => sub { 'bar2' },
+  );
 
 =head1 AUTHOR
 
---
+++ lib/MouseX/SingletonMethod/Role.pm
@@ -0,0 +1,32 @@
+package MouseX::SingletonMethod::Role;
+use Mouse::Role;
+
+my $singleton = sub {
+    my $self = shift;
+    my $methods = shift || {};
+
+    my $meta = $self->meta->create_anon_class(
+        superclasses => [ $self->meta->name ],
+        methods      => $methods,
+    );
+    $meta->add_method( meta => sub {$meta} );
+
+    bless $self, $meta->name;
+};
+
+sub become_singleton {
+    $_[0]->$singleton;
+}
+
+sub add_singleton_method {
+    $_[0]->$singleton( { $_[1] => $_[2] } );
+}
+
+sub add_singleton_methods {
+    my $self = shift;
+    $self->$singleton( {@_} );
+}
+
+no Mouse::Role;
+1;
+__END__
